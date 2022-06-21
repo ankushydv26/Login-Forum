7cc32f9ec1335be9ad6f353bbee7b50bad62720c
ANKUSH <ankush.yadav@aripratech.com>
2022-06-21 12:06:43 +0530

    homepage link change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Login from "./Page/Login";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
